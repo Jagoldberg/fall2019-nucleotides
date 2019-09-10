9e54c490718999252eaf0938246c857125db5cad
Jackson Goldberg <jagoldberg@chapman.edu>
2019-09-10 14:28:44 -0700

    initial commit

---
+++ DNA.cpp
@@ -0,0 +1,12 @@
+#include "DNA.h"
+#include <iostream>
+
+using namespace std;
+
+DNA::DNA(string s){
+  
+}
+
+DNA::~DNA(){
+  cout << "object deleted" << endl;
+}
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "DNA.h"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char** argv){
+  ofstream DNAlist;
+  DNAlist.open(argv[1]);
+}
